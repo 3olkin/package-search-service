fcdbb8c609231628d2383f8e06afb6b050649c82
Z4RX <iamz4rx@gmail.com>
2021-01-27 04:40:12 +0300

    test db connect + deploy auto migrate

--- main.go
+++ main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jinzhu/gorm"
 
 	"github.com/z4rx/search-service/Config"
+	"github.com/z4rx/search-service/Models"
 	"github.com/z4rx/search-service/Routes"
 )
 
 var err error
 
 func main() {
-	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	Config.DB, err = gorm.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Println("Status:", err)
 	}
 	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.Distro{}, &Models.Package{}, &Models.Matching{})
 	r := Routes.SetupRouter()
 	r.Run()
 }
